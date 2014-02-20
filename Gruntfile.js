32b47c9ce35895da16e4f43c0ec82c932d7960e6
breno.polanski <breno.polanski@gmail.com>
2014-02-20 14:51:11 -0300

    Added file Gruntfile.js

---
+++ Gruntfile.js
@@ -0,0 +1,22 @@
+module.exports = function (grunt) {
+	grunt.initConfig({
+		autoprefixer: {
+			file: {
+				src: 'slim-helper.css'
+			}
+		},
+		cssmin: {
+			options: {
+				banner: '/* Copyright 2014, All Rights Reserved. https://github.com/brenopolanski/slim-helper-css/ */'
+			},
+			dist: {
+				src: 'slim-helper.css',
+				dest: 'slim-helper.min.css'
+			}
+		}
+	});
+
+	grunt.loadNpmTasks('grunt-autoprefixer');
+	grunt.loadNpmTasks('grunt-contrib-cssmin');
+	grunt.registerTask('default', ['autoprefixer', 'cssmin']);
+};
